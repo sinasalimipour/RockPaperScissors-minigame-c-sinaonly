b333ca226e141c85c7bd8152a04e9611f8ff2268
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-10 18:25:46 +0430

    Add project files.

---
+++ rock paper siser/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+  
+      
+        
+
+         
+ namespace rock_paper_siser
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string rock = "rock";
+                string paper = "paper";
+                string siser = "siser";
+
+                Console.WriteLine("rock paper siser");
+                Console.WriteLine("your chios");
+                string useransewr = Convert.ToString(Console.Read());
+
+                if (useransewr == rock || useransewr == siser || useransewr == paper)
+                {
+                    Console.WriteLine("you chois is" + useransewr);
+                }
+                else { Console.WriteLine("you did not chois coractle "); }
+
+                Random random = new System.Random();
+                int a = random.Next(2);
+
+
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+
+    
